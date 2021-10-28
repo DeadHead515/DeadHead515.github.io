9824e8194516a63e284b16ca72de4212deba5ff0
kjurgens <kjurgens@lightedge.com>
2021-10-28 17:03:52 -0500

    Background image attemps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import logo_ from './svg/logo_.svg';
+import vader from './media/vader.jpeg';
 import './App.css';
 
+const bgStyle = {
+    backgroundImage: `url(${vader})`,
+    backgroundSize: 'cover',
+    height: '100%',
+    width: '100%'
+};
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <div className='background-image'>
+            <h1>Hello Jedi... </h1>
+        </div>
   );
 }
-
+// <div className="App">
+//     {/*<header className="App-header">*/}
+//     {/*  /!*<img src={logo_} className="App-logo" alt="logo" />*!/*/}
+//     {/*  /!*<p>*!/*/}
+//     {/*  /!*  Welcome Jedi.*!/*/}
+//     {/*  /!*</p>*!/*/}
+//     {/*</header>*/}
+//     <div className='background-image' >
+//         <div>
+//             <h1>Hello Jedi... </h1>
+//         </div>
+//     </div>
+// </div>
 export default App;
