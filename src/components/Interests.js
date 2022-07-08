c2badd2dc4f3b400a4cd878104ca3d561fc9f40a
kjurgens <kjurgens@lightedge.com>
2022-07-07 21:59:07 -0500

    Adding Icons for interest list.

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,4 +1,12 @@
 import { Container, ListGroup, ListGroupItem, Row } from "react-bootstrap";
+import HikingIcon from '@mui/icons-material/Hiking';
+import OutdoorGrillIcon from '@mui/icons-material/OutdoorGrill';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import PhishingIcon from '@mui/icons-material/Phishing';
+import SportsFootballIcon from '@mui/icons-material/SportsFootball';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import GolfCourseIcon from '@mui/icons-material/GolfCourse';
+import TwoWheelerIcon from '@mui/icons-material/TwoWheeler';
 
 export const Interests= () => {
     return(
@@ -7,18 +15,18 @@ export const Interests= () => {
                     Things I like to Do:
                 </Row>
                 <div style={{display: 'flex', flexDirection:'row', justifyContent: 'center', }}>
-                    <ul>
-                        <li>Fishing</li>
-                        <li>Football</li>
-                        <li>Guitar</li>
-                        <li>Biking</li>
+                    <ul style={{marginRight: "15px"}}>
+                        <li><PhishingIcon/> Fishing</li>
+                        <li><SportsFootballIcon/> Football</li>
+                        <li><LibraryMusicIcon/> Guitar</li>
+                        <li><TwoWheelerIcon/> Biking</li>
                     </ul>
                 
-                    <ul>
-                        <li>Disc Golf</li>
-                        <li>Reading</li>
-                        <li>Hiking</li>
-                        <li>Cooking food</li>
+                    <ul style={{marginLeft: "15px"}}>
+                        <li><GolfCourseIcon /> Disc Golf</li>
+                        <li><AutoStoriesIcon/> Reading</li>
+                        <li><HikingIcon/> Hiking</li>
+                        <li><OutdoorGrillIcon/> Cooking food</li>
                     </ul> 
                 </div>
             </Container>
